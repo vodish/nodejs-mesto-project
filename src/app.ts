339a705ab85911d1cfa958154a56d2a16a26b4d2
pkarasev <p.karasev@psw.ru>
2024-04-09 14:58:03 +0300

    upd

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-// import path from 'path';
+import express from 'express';
+import 'dotenv/config';
 
-console.log('файл', __filename)
+const SERVER_PORT = process.env.SERVER_PORT || 3001;
+
+
+
+const app = express();
+
+app.listen(+SERVER_PORT);
+
+console.log(`Server run at http://localhost:${SERVER_PORT}`);
