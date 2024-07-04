3812356f79c682ff7f8f531e0d2a29a04dc69aa5
pkarasev <p.karasev@psw.ru>
2024-07-04 14:56:34 +0300

    upd

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import UserModel from '../models/userModel';
 
 export function testTest(req: Request, res: Response) {
-  res.json({ data: 'сервер работает!' });
+  res.json({ data: 'сервер работает: ' + Date.now() });
 }
 
 export function mongoTest(req: Request, res: Response, next: NextFunction) {
